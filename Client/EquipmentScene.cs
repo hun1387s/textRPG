46513270943286bbbe061e5742c600f22c9e56ed
Lee Sanghun <hun1387s@gmail.com>
2025-02-03 21:11:17 +0900

    장비 관리 토글

--- Client/EquipmentScene.cs
+++ Client/EquipmentScene.cs
@@ -17,18 +17,24 @@ namespace Client
                 instance = new EquipmentScene();
             return instance;
         }
+        List<int> equipIdx;
+        Core core = Core.GetInst();
 
         public override void Enter()
         {
             Clear();
+            
             nextScene = this;
+            
+            equipIdx = new List<int>();
 
             Console.WriteLine("인벤토리 - 장착관리");
             Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
             Console.WriteLine("[아이템 목록]\n");
+            printItem();
 
 
-            Console.WriteLine("0. 나가기");
+            Console.WriteLine("\n\n0. 나가기");
 
             string result = Console.ReadLine();
 
@@ -37,6 +43,36 @@ namespace Client
                 case "0":
                     // InventoryScene
                     nextScene = InventoryScene.GetInst();
+                    break;
+
+                case "1":
+                    // 1번 아이템 장착 토글
+                    SetItemToggle(equipIdx[0]);
+                    break;
+
+                case "2":
+                    // 2번 아이템 장착 토글
+                    SetItemToggle(equipIdx[1]);
+                    break;
+
+                case "3":
+                    // 3번 아이템 장착 토글
+                    SetItemToggle(equipIdx[2]);
+                    break;
+
+                case "4":
+                    // 4번 아이템 장착 토글
+                    SetItemToggle(equipIdx[3]);
+                    break;
+
+                case "5":
+                    // 5번 아이템 장착 토글
+                    SetItemToggle(equipIdx[4]);
+                    break;
+
+                case "6":
+                    // 6번 아이템 장착 토글
+                    SetItemToggle(equipIdx[5]);
                     break;
 
                 default:
@@ -50,6 +86,63 @@ namespace Client
         public override Scene Exit()
         {
             return nextScene;
+        }
+
+        private void printItem()
+        {
+            Core core = Core.GetInst();
+            // item 출력
+            int count = 0;
+            for (int i = 0; i < core.items.Count; i++)
+            {
+                // 장비 장착 옵션
+                string equip = "   ";
+                // 공격력 or 방어력
+                string AorD = "";
+                // 가지고 있는 장비
+                if (core.items[i].Own)
+                {
+                    equipIdx.Add(i);
+                    count++;
+                    // 장비 장착 유무
+                    if (core.items[i].Equip)
+                    {
+                        equip = "[E]";
+                    }
+
+                    // 방어 장비
+                    if (core.items[i].Defense > 0)
+                    {
+                        AorD = $"방어력 +{core.items[i].Defense}";
+                    }
+                    // 공격 장비
+                    else if (core.items[i].Attack > 0)
+                    {
+                        AorD = $"공격력 +{core.items[i].Attack}";
+                    }
+
+                    Console.WriteLine($" -{count} {equip}{core.items[i].Name} | {AorD} | {core.items[i].Description}");
+                }
+            }
+        }
+
+        private void SetItemToggle(int idx)
+        {
+            bool equip = core.items[idx].Equip ? false : true;
+
+            Character character = Character.GetInst();
+            if (equip)
+            {
+                character.Attack += core.items[idx].Attack;
+                character.Defense += core.items[idx].Defense;
+            }
+            else
+            {
+                character.Attack -= core.items[idx].Attack;
+                character.Defense -= core.items[idx].Defense;
+            }
+
+            core.items[idx].Equip = equip;
         }
     }
 }
