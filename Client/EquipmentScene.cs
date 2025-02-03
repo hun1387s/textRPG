bd5650c63d5a0ea4914689465915e2d8f5e7b7b9
Lee Sanghun <hun1387s@gmail.com>
2025-02-03 17:19:20 +0900

    Create 장비, 인벤토리 Scene

---
+++ Client/EquipmentScene.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class EquipmentScene : Scene
+    {
+        private EquipmentScene() { }
+        private static EquipmentScene? instance;
+        public static EquipmentScene GetInst()
+        {
+            if (instance == null)
+                instance = new EquipmentScene();
+            return instance;
+        }
+
+        public override void Enter()
+        {
+
+        }
+
+        public override Scene Exit()
+        {
+            return nextScene;
+        }
+    }
+}
---
+++ Client/InventoryScene.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class InventoryScene : Scene
+    {
+        private InventoryScene() { }
+        private static InventoryScene? instance;
+        public static InventoryScene GetInst()
+        {
+            if (instance == null)
+                instance = new InventoryScene();
+            return instance;
+        }
+
+        public override void Enter()
+        {
+
+        }
+
+        public override Scene Exit()
+        {
+            return nextScene;
+        }
+    }
+}
