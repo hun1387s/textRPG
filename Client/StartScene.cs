4dcf6d6bc2b8f705a4bfdee3ccdfe63b10923de2
Lee Sanghun <hun1387s@gmail.com>
2025-02-03 17:35:24 +0900

    던전 들어가기 버튼 추가 및 이름 변경 기능 추가

--- Client/StartScene.cs
+++ Client/StartScene.cs
@@ -9,6 +9,7 @@ namespace Client
 {
     internal class StartScene : Scene
     {
+        // Singleton
         private StartScene() { }
         private static StartScene? instance;
         public static StartScene GetInst()
@@ -22,7 +23,7 @@ namespace Client
         {
             Clear();
             nextScene = this;
-            //Character character = Character.GetInst();
+            Character character = Character.GetInst();
 
             Console.WriteLine("RPG 마을에 오신 여러분 환영합니다.");
             Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.\n");
@@ -30,6 +31,10 @@ namespace Client
             Console.WriteLine("1. 상태보기");
             Console.WriteLine("2. 인벤토리");
             Console.WriteLine("3. 상점");
+            Console.WriteLine("4. 던전 들어가기");
+
+            Console.WriteLine("5. 이름변경");
+
 
             string result = Console.ReadLine();
 
@@ -47,8 +52,18 @@ namespace Client
                     // EquipmentScene
                     nextScene = EquipmentScene.GetInst();
                     break;
+                case "5":
+                    // 이름 변경
+                    Clear();
+                    Console.WriteLine("변경할 이름을 입력하세요.");
+                    character.Name = Console.ReadLine();
+
+                    Console.WriteLine($"이름이 '{character.Name}'로 변경되었습니다.");
+                    Thread.Sleep(1000);
+                    break;
 
                 default:
+                    // 지금 Scene 재반환 및 화면 정리
                     Console.WriteLine("올바른 값을 입력하세요.");
                     Thread.Sleep(1000);
                     break;
