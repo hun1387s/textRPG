6597251fdaa9e08a684d00498883fac4988e56b6
Lee Sanghun <hun1387s@gmail.com>
2025-02-03 21:11:01 +0900

    인벤토리 장비 출력

--- Client/InventoryScene.cs
+++ Client/InventoryScene.cs
@@ -26,9 +26,11 @@ namespace Client
             Console.WriteLine("인벤토리");
             Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
             Console.WriteLine("[아이템 목록]\n");
+            printItem();
 
-            Console.WriteLine("1. 장착 관리");
+            Console.WriteLine("\n\n1. 장착 관리");
             Console.WriteLine("0. 나가기");
+
 
             string result = Console.ReadLine();
 
@@ -54,6 +56,43 @@ namespace Client
         public override Scene Exit()
         {
             return nextScene;
+        }
+
+        private void printItem()
+        {
+            Core core = Core.GetInst();
+            // item 출력
+            int count = 0;
+            for (int i = 0; i < core.items.Count; i++)
+            {
+                // 장비 장착 옵션
+                string equip = "   ";
+                // 공격력 or 방어력
+                string AorD = "";
+                if (core.items[i].Own)
+                {
+                    // 방어 장비
+                    if (core.items[i].Defense > 0)
+                    {
+                        AorD = $"방어력 +{core.items[i].Defense}";
+                    }
+                    // 공격 장비
+                    else if (core.items[i].Attack > 0)
+                    {
+                        AorD = $"공격력 +{core.items[i].Attack}";
+                    }
+                    // 장비 장착 유무
+                    if (core.items[i].Equip)
+                    {
+                        count++;
+                        equip = "[E]";
+                        Console.WriteLine($" -{count} {equip}{core.items[i].Name} | {AorD} | {core.items[i].Description}");
+                    }
+
+                    
+                }
+            }
+            
         }
     }
 }
